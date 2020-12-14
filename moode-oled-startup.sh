125d984b4eb5f6d8ba498f7340b41f53357f126e
duracell80 <lee.jordan@gmail.com>
2020-12-13 22:05:02 -0500

    Adding Station Tuning Knob to Startup

--- moode-oled-startup.sh
+++ moode-oled-startup.sh
@@ -2,6 +2,7 @@
 
 #ps -ef | grep "python /home/pi/MoodeAudio-OLED/moode-oled.py" | grep -v grep | awk '{print $2}' | xargs kill
 
+sudo python /home/pi/MoodeAudio-OLED/rotseek.py &
 
 sudo python /home/pi/MoodeAudio-OLED/moode-oled-splash.py
 
